104fa5e7e3a7598a9fe923059ef42027cbac0722
gatere <mercynjerigatere@gmail.com>
2024-04-12 02:06:03 -0400

    Adds js data

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-console.log("mercy");
+
 document.addEventListener('DOMContentLoaded',() => {
 
-    fetchData()
+    fetchBooks()
 })
 
-function fetchData() {
-    fetch('http://localhost:3000/data')
-       .then(res => res.json())
-       .then((boo) => {
-    console.log((boo));
-})
+function fetchBooks() {
+    fetch(`http://localhost:3000/books`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then((res) => res.json())
+    .then((book) => {
+        book.forEach((book1) => renderBooks(book1));
+    })
+    
+}
+
+function renderBooks(book1) {
+    const booksContainer = document.querySelector('#book')
+const parentDiv = document.createElement('div')
+parentDiv.classList.add('card')
+    
+const image = document.createElement('img')
+image.classList.add('card-img-top', 'mt-2')
+image.height = 200;
+image.src = book1.imageLink
+image.alt = book1.title
+
+
+parentDiv.appendChild(image)
+
+booksContainer.appendChild(parentDiv);
 }
